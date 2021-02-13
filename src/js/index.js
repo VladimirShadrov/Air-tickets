dd87e4005f80a9b19f19ea9a67130fe00c1ad2fa
Vladimir <shadrov_vladimir@mail.ru>
2021-02-13 16:23:04 +0300

    Добавлена верстка карточки авиарейса

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 import '../styles/styles.scss';
 import '../styles/filters.scss';
+import '../styles/ticket.scss';
+
+
+// Перенос изображений и шрифтов в папку "dist"
+require.context('../images', true, /\.(png|jpg|svg|gif)$/);
